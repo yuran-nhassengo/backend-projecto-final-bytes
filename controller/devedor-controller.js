029d7e30ed77b83a03736992963da33c5bef492c
yurannhassengo <yurannhassengo@gmail.com>
2024-08-01 14:45:37 -0700

    Add GetAllDebt

--- controller/devedor-controller.js
+++ controller/devedor-controller.js
@@ -1,5 +1,24 @@
 const asyncHandler = require('express-async-handler');
-const credor = require("../model/devedor-model");
+const devedorModel = require("../model/devedor-model");
 const {default:mongoose} = require("mongoose");
+
+const getAllDevedor = asyncHandler(async (req,res) =>{
+    const devedores = await devedorModel.find();
+
+    const usData = devedores.map(devedor =>{
+        return {
+            id: devedor._id,
+            name: devedor.name,
+            email:devedor.email,
+            data:devedor.data,
+            genero:devedor.genero,
+            contacto:devedor.contacto
+        }
+    });
+
+    res.status(200).json({data:usData})
+});
+
+
 
 
